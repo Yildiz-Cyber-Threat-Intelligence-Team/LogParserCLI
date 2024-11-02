c929bbdc4fbe178b81c99908ad53f71683102ba4
Enes <91144980+hypervsec@users.noreply.github.com>
2024-11-02 16:01:44 +0100

    Update txt_search.go

--- parsers/txt_search.go
+++ parsers/txt_search.go
@@ -5,26 +5,26 @@ import (
 	"strings"
 )
 
-// TXT içindeki satırları tarar ve anahtar kelimenin geçtiği yerleri bulur
+
 func SearchInTXT(data []string, keyword string) {
 	var found bool
 	for i := 0; i < len(data); i++ {
-		// Eğer satırda URL varsa ve anahtar kelime mevcutsa
+		
 		if strings.Contains(data[i], "URL:") && strings.Contains(data[i], keyword) {
 			found = true
 			fmt.Println("Found the following information:")
 			fmt.Println("===============================")
-			fmt.Println(data[i]) // URL satırını yazdır
+			fmt.Println(data[i])
 
-			// Username, Password ve Application bilgilerini bul
+			
 			if i+1 < len(data) {
-				fmt.Println(data[i+1]) // Username
+				fmt.Println(data[i+1]) 
 			}
 			if i+2 < len(data) {
-				fmt.Println(data[i+2]) // Password
+				fmt.Println(data[i+2]) 
 			}
 			if i+3 < len(data) {
-				fmt.Println(data[i+3]) // Application
+				fmt.Println(data[i+3]) 
 			}
 			fmt.Println("===============================")
 		}
